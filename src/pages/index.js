fd79e5c35bce2cc3d6078549ac51e0acc581d443
FHTsung <109931260+FHTsung@users.noreply.github.com>
2022-07-28 22:53:09 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,7 +83,7 @@ const IndexPage = () => (
         style={{ marginBottom: `var(--space-3)` }}
       />
       <h1>
-        Welcome to <b>Gatsby!</b>
+        Prajna Dharma with <b>Jianhu Shi</b>
       </h1>
       <p className={styles.intro}>
         <b>Example pages:</b>{" "}
